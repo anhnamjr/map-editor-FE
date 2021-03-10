9bcf86b63b1b7b8a829d83fe884b0211db8a71ef
Phuc Manh <phucmanh1999@gmail.com>
2021-03-10 09:25:35 +0700

    add page signin, signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,173 @@
-import React from "react";
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import './style.css';
+import { Form, Input, Select, Checkbox, Button } from 'antd';
+const { Option } = Select;
 
-export default function SingUp() {
-  return <div>SingUp</div>;
+const formItemLayout = {
+  labelCol: {
+    
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 3,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+
+
+
+export default function SignUp() {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  }
+    const prefixSelector = (
+      <Form.Item name="prefix" noStyle>
+        <Select
+          style={{
+            width: 70,
+          }}
+        >
+          <Option value="84">+84</Option>
+          <Option value="87">+87</Option>
+        </Select>
+      </Form.Item>
+    );
+  
+  return (
+    <Form
+    {...formItemLayout}
+    form={form}
+    name="register"
+    onFinish={onFinish}
+    initialValues={{
+      prefix: '84',
+    }}
+    scrollToFirstError
+    >
+      <h1>Sign up</h1>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone number!',
+          },
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: '100%',
+          }}
+        />
+      </Form.Item>
+
+
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+
 }
