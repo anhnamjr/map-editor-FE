fb564e9d82e8e9ec25c020de0c8ae5c895b33246
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2021-04-18 20:55:01 +0700

    add print, printScreen

--- src/pages/Maps/index.jsx
+++ src/pages/Maps/index.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect, useRef } from "react";
 import "antd/dist/antd.css";
 import "leaflet-draw/dist/leaflet.draw.css";
-import { Map, TileLayer, ZoomControl } from "react-leaflet";
+import { Map, TileLayer, ZoomControl, withLeaflet } from "react-leaflet";
 import MapSidebar from "../../components/MapSidebar";
 import { useSelector } from "react-redux";
 import MapLayerControl from "../../components/MapLayerControl";
 import Draw from "../../components/Draw";
+import PrintControlDefault from "react-leaflet-easyprint";
+const PrintControl = withLeaflet(PrintControlDefault);
 
 const Maps = () => {
   const [geoData, setGeoData] = useState({});
   const data = useSelector((state) => state.layerReducer.layerData);
-  const mapRef = useRef()
+  const mapRef = useRef();
+  const printControlRef = useRef();
 
-  const { center } = useSelector((state) => state.mapReducer)
+  const { center } = useSelector((state) => state.mapReducer);
 
   useEffect(() => {
     setGeoData(data);
@@ -21,13 +24,31 @@ const Maps = () => {
   return (
     <>
       <MapSidebar />
-      <Map className="mapStyle" zoom={13} center={center || [10.7646598, 106.6855794]} ref={mapRef}>
+      <Map
+        className="mapStyle"
+        zoom={13}
+        center={center || [10.7646598, 106.6855794]}
+        ref={mapRef}
+      >
         <MapLayerControl mapRef={mapRef} />
         <ZoomControl position="topright" />
         <Draw geoData={geoData} />
         <TileLayer
           attribution=""
           url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+        />
+        <PrintControl
+          ref={printControlRef}
+          position="topright"
+          sizeModes={["Current", "A4Portrait", "A4Landscape"]}
+          hideControlContainer={false}
+        />
+        <PrintControl
+          position="topright"
+          sizeModes={["Current", "A4Portrait", "A4Landscape"]}
+          hideControlContainer={false}
+          title="Export as PNG"
+          exportOnly
         />
       </Map>
     </>
