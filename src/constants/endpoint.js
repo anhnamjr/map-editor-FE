df34c9a2a1cc3b98f823969cc89540bc526a3ddc
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2021-06-16 23:59:45 +0700

    fix https

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = "http://192.168.15.138:3000";
 // export const BASE_URL = "http://localhost:3001/user";
 // export const AUTH_URL = "http://localhost:3001/auth";
-export const BASE_URL = "http://map-online.herokuapp.com/user";
-export const AUTH_URL = "http://map-online.herokuapp.com/auth";
+export const BASE_URL = "https://map-online.herokuapp.com/user";
+export const AUTH_URL = "https://map-online.herokuapp.com/auth";
 // export const BASE_URL = "http://192.168.15.139:3001/user";
 // export const AUTH_URL = "http://192.168.15.139:3001/auth";
